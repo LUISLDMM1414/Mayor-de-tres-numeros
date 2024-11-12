8d756f204de63dfa7062af7f7f15c19f85be1d6c
LUISLDMM1414 <moreno.luisda01@gmail.com>
2024-11-11 23:30:44 -0600

    Add files via upload

---
+++ # Solicitar los tres números al usuario.py
@@ -0,0 +1,15 @@
+# Solicitar los tres números al usuario
+num1 = float(input("Introduce el primer número: "))
+num2 = float(input("Introduce el segundo número: "))
+num3 = float(input("Introduce el tercer número: "))
+
+# Encontrar el número mayor usando condicionales
+if num1 >= num2 and num1 >= num3:
+    mayor = num1
+elif num2 >= num1 and num2 >= num3:
+    mayor = num2
+else:
+    mayor = num3
+
+# Mostrar el número mayor
+print("El número mayor es:", mayor)
